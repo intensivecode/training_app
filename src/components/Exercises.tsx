29f0fe3049361d1a814d3844ba3d9046f7738551
aladinelkhalil <aladin@intensivecode.se>
2024-04-27 09:33:47 +0400

    absolute positioning

--- src/components/Exercises.tsx
+++ src/components/Exercises.tsx
@@ -12,9 +12,15 @@ export default function Exercises({ exercises }: Props) {
         <Link
           to={`/exercises/${exercise.id}`}
           key={exercise.id}
-          className="grid place-items-center w-48 h-32 text-white text-xl font-bold bg-primary rounded-box hover:scale-110 cursor-pointer transition-transform ease-in-out duration-300"
+          className="relative grid place-items-center w-48 h-32 text-white text-xl font-bold bg-primary bg-opacity-100 rounded-box cursor-pointer hover:bg-opacity-80"
         >
           {exercise.name}
+          <div className="absolute top-2 right-2">
+            <i className="fas fa-shopping-cart" />
+            <div className="absolute bottom-0 right-0 grid place-items-center w-4 h-4 rounded-full bg-red-700">
+              <span className="text-white text-xs">1</span>
+            </div>
+          </div>
         </Link>
       ))}
     </div>
